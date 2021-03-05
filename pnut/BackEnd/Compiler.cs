e9935d4ade2e57df11f655c2f0ea675d2a72eaba
Vesk <veskmail@gmail.com>
2021-03-05 17:09:02 +0200

    Created the Visual Studio Project

---
+++ pnut/BackEnd/Compiler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace pnut
+{
+	class Compiler
+	{
+		// To Do: Everything...
+		public static void Compile(string source_file)
+		{
+			Debug.WriteLine(source_file);
+		}
+	}
+}
