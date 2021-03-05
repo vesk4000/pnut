b038c6d1660dbbaee04d8c4b6fb7e9ed8391537d
Vesk <veskmail@gmail.com>
2021-03-05 17:38:37 +0200

    Added a scrollbar...

--- pnut/User Interface/Form1.Designer.cs
+++ pnut/User Interface/Form1.Designer.cs
@@ -30,6 +30,9 @@ namespace pnut
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -42,14 +45,31 @@ namespace pnut
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.vScrollBar1);
+			this.panel1.Location = new System.Drawing.Point(13, 43);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(866, 445);
+			this.panel1.TabIndex = 1;
+			// 
+			// vScrollBar1
+			// 
+			this.vScrollBar1.Location = new System.Drawing.Point(751, 4);
+			this.vScrollBar1.Name = "vScrollBar1";
+			this.vScrollBar1.Size = new System.Drawing.Size(24, 391);
+			this.vScrollBar1.TabIndex = 0;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(916, 518);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -57,6 +77,8 @@ namespace pnut
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.VScrollBar vScrollBar1;
 	}
 }
 
