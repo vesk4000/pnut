3945a2ad2b96f5d08ec8a3c917f4e97acb22667e
Vesk <veskmail@gmail.com>
2021-03-05 19:07:19 +0200

    did some changez

--- pnut/User Interface/Form1.Designer.cs
+++ pnut/User Interface/Form1.Designer.cs
@@ -30,55 +30,46 @@ namespace pnut
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-			this.panel1.SuspendLayout();
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(13, 13);
+			this.button1.Location = new System.Drawing.Point(12, 165);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.Size = new System.Drawing.Size(90, 26);
 			this.button1.TabIndex = 0;
 			this.button1.Text = "Compile";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
-			// panel1
+			// label1
 			// 
-			this.panel1.Controls.Add(this.vScrollBar1);
-			this.panel1.Location = new System.Drawing.Point(13, 43);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(866, 445);
-			this.panel1.TabIndex = 1;
-			// 
-			// vScrollBar1
-			// 
-			this.vScrollBar1.Location = new System.Drawing.Point(751, 4);
-			this.vScrollBar1.Name = "vScrollBar1";
-			this.vScrollBar1.Size = new System.Drawing.Size(24, 391);
-			this.vScrollBar1.TabIndex = 0;
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(71, 17);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Compiler: ";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(916, 518);
-			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
-			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.VScrollBar vScrollBar1;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
