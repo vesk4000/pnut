0b72c1175c9bfdfb3d49d6303763ce2d1b7b1461
GreenUncle <quickfixop@gmail.com>
2021-06-23 23:08:18 +0300

    added problem command class

---
+++ pnut/CLI/Commands/Problem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pnut.Commands
+{
+    class Problem : Command
+    {
+        public Problem() :
+            base("problem",
+            "                        ",
+            @"Arguments:
+              <name (string)> - name of the problem")
+
+        { }
+
+        public override void Run(string[] args)
+        {
+            // check for # of args
+            // check for validity of entered shits    tryparse or some shit
+            // file even exists bro?
+            // ml int?
+            // just use try catch and cout some errors
+        }
+    }
+}
